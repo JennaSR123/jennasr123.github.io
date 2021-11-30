274f4f79a0f6976525ddcfaedf2fd8cd1894582c
JennaSR123 <56770735+JennaSR123@users.noreply.github.com>
2021-11-30 18:11:03 -0500

    Delete js directory

--- js/navhover.js
+++
@@ -1,9 +0,0 @@
-function showNav() {
-"use strict";
-  var x = document.getElementById('nav');
-    if (x.style.height === '238px') {
-        x.style.height = '60px';
-    } else {
-        x.style.height = '238px';
-    }
-}
